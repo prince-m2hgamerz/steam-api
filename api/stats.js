81b61f5d0abebab3de396d8eadde56da94623a2c
Prince kumar <prince.m2hgamerz@gmail.com>
2025-12-04 13:18:05 +0530

    update stats

--- api/stats.js
+++ api/stats.js
@@ -2,16 +2,31 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = (req, res) => {
-  const file = path.join(process.cwd(), "data", "accounts.json");
-  const accounts = JSON.parse(fs.readFileSync(file, "utf8"));
+  try {
+    const filePath = path.join(process.cwd(), "data", "accounts.json");
+    const accounts = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
-  const totalAccounts = accounts.length;
-  const totalGames = accounts.reduce((a, b) => a + b.total_games, 0);
+    const totalAccounts = accounts.length;
+    const totalGames = accounts.reduce((sum, acc) => sum + (acc.total_games || 0), 0);
 
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).json({
-    success: true,
-    totalAccounts,
-    totalGames
-  });
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({
+      success: true,
+      totalAccounts,
+      totalGames,
+      countries: accounts.reduce((map, acc) => {
+        const c = acc.country || "UNKNOWN";
+        map[c] = (map[c] || 0) + 1;
+        return map;
+      }, {})
+    });
+
+  } catch (err) {
+    console.error("Stats API Error:", err);
+    res.status(500).json({
+      success: false,
+      message: "Failed to load statistics.",
+      error: String(err)
+    });
+  }
 };
